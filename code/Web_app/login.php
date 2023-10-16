0f2b093fa7c8669405d5f80c0403f6bd574f1514
Swaranjali <swaranjali1617@gmail.com>
2023-10-16 18:50:30 -0400

    Emaiil validation for login

--- code/Web_app/login.php
+++ code/Web_app/login.php
@@ -15,6 +15,10 @@
       header('Location: home.php');
   }
   include "connectDB.php";
+  if (isset($_POST['submit'])) {
+    if ($_POST['inputEmail'] === "") {
+        echo '<div class="alert alert-danger text-center small-box">Email is required</div>';
+    } 
  ?>
 <body>
   <div class="bg">
