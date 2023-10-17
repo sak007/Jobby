3652d2fbe09095a6dfd15872c1ffe66bcaa5d479
Swaranjali <swaranjali1617@gmail.com>
2023-10-17 17:40:49 -0400

    changing background styles

--- code/Web_app/forgot.php
+++ code/Web_app/forgot.php
@@ -15,7 +15,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 
     <link rel="icon" href="Favicon.png">
 
--- code/Web_app/reset-password.php
+++ code/Web_app/reset-password.php
@@ -17,7 +17,7 @@ include('connectDB.php');
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 
     <link rel="icon" href="Favicon.png">
 
