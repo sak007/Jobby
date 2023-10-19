9e170eb23c674fe18ab4577f30a12234cf417c69
Swaranjali <swaranjali1617@gmail.com>
2023-10-19 11:20:35 -0400

    Changes in UI

--- code/Web_app/home.php
+++ code/Web_app/home.php
@@ -1,106 +1,111 @@
- <!DOCTYPE html>
- <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
- <head>
-   <meta charset="utf-8">
-   <meta name="author" content="Group-32, Fall 2021">
-   <meta name="viewport" content="width=device-width,initial-scale=1">
-   <title>JOBBY- Home</title>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-   <link rel="stylesheet" href="css/styles.css">
- </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Group-32, Fall 2021">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>JOBIFY- Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
 
- <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
- </head>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
- <body>
+<body>
 
-   <div class="header">
-     <img src="logo.jpg" alt="logo">
-     <div class="header-right">
-       <a class="active" href="home.php">Home</a>
-       <a href="register.php">Job Board</a>
-       <a href="updateResume.php">Upload Resume</a>
-       <a href="logout.php">Logout</a>
-     </div>
-   </div>
+  <div class="header">
+    <img src="logos.jpg" alt="logo">
+    <div class="header-right">
+      <a class="active" href="home.php">Home</a>
+      <a href="register.php">Job Board</a>
+      <a href="updateResume.php">Upload Resume</a>
+      <a href="logout.php">Logout</a>
+    </div>
+  </div>
 
- </body>
- <?php
-  session_start();
-  if (!isset($_SESSION['user'])) {
-    header('Location: login.php');
-  }
-  include "connectDB.php";
-  $user_email = $_SESSION['user'];
-  $user = $user_email;
-  // echo "User Job Boards";
-  $userJbQuery = "select jb.name, jb.job_board_id from job_board jb, user_notification un, user_master um where jb.job_board_id = un.job_board_id and un.user_id = um.user_id and um.user_email = '" . $user_email . "'";
-  $userJbResult = $conn->query($userJbQuery);
-  $job_board_array = array();
-  $job_board_ids = array();
-  while ($row = $userJbResult->fetch_assoc()) {
-    array_push($job_board_array, $row["name"]);
-    array_push($job_board_ids, $row["job_board_id"]);
-    // echo $job_board_array[0];
-    // echo "<br>";
-  }
-  ?>
+</body>
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+  header('Location: login.php');
+}
+include "connectDB.php";
+$user_email = $_SESSION['user'];
+$user = $user_email;
+// echo "User Job Boards";
+$userJbQuery = "select jb.name, jb.job_board_id from job_board jb, user_notification un, user_master um where jb.job_board_id = un.job_board_id and un.user_id = um.user_id and um.user_email = '" . $user_email . "'";
+$userJbResult = $conn->query($userJbQuery);
+$job_board_array = array();
+$job_board_ids = array();
+while ($row = $userJbResult->fetch_assoc()) {
+  array_push($job_board_array, $row["name"]);
+  array_push($job_board_ids, $row["job_board_id"]);
+  // echo $job_board_array[0];
+  // echo "<br>";
+}
+?>
 
- <body>
-   <div class="bg">
-     <section class="h-100">
-       <div class="container h-100">
-         <div class="row justify-content-sm-center h-100">
-           <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-             <div class="text-center my-5">
-             </div>
+<body>
+  <div class="bg">
+    <section class="h-100">
+      <div class="container h-100">
+        <div class="row justify-content-sm-center h-100">
+          <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div class="text-center my-5">
+            </div>
 
-             <div class="card shadow-lg">
-               <div class="card-body p-5">
-                 <h1 class="fs-4 card-title fw-bold mb-4">Welcome to Jobby!</h1>
-                 <form method="POST" action="" enctype="multipart/form-data" class="needs-validation" novalidate="" autocomplete="off">
-                   <?php
+            <div class="card shadow-lg">
+              <div class="card-body p-5">
+                <h1 class="fs-4 card-title fw-bold mb-4">Welcome to Jobify!</h1>
+                <form method="POST" action="" enctype="multipart/form-data" class="needs-validation" novalidate=""
+                  autocomplete="off">
+                  <?php
 
-                    // Get User Details from the database
-                    $user_email = $_SESSION['user'];
-                    $userNameQuery = "select um.user_fname, um.user_lname from user_master um where um.user_email = '" . $user_email . "'";
-                    $userNameResult = $conn->query($userNameQuery);
-                    $namearray = array();
+                  // Get User Details from the database
+                  $user_email = $_SESSION['user'];
+                  $userNameQuery = "select um.user_fname, um.user_lname from user_master um where um.user_email = '" . $user_email . "'";
+                  $userNameResult = $conn->query($userNameQuery);
+                  $namearray = array();
 
-                    while ($row = $userNameResult->fetch_assoc()) {
-                      array_push($namearray, $row["user_fname"]);
-                      array_push($namearray, $row["user_lname"]);
-                    }
+                  while ($row = $userNameResult->fetch_assoc()) {
+                    array_push($namearray, $row["user_fname"]);
+                    array_push($namearray, $row["user_lname"]);
+                  }
 
-                    // Display User Details
-                    // Display User Details
-                    echo "<td width='80' style='text-align:center;'>";
-                    echo "<b>Name</b> &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;" . ucfirst($namearray[0]) . "<br>";
-                    echo "<b>User Email</b>\t\t\t&emsp;", $user_email;
-                    echo "</td>";
+                  // Display User Details
+                  // Display User Details
+                  echo "<td width='80' style='text-align:center;'>";
+                  echo "<b>Name</b> &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;" . ucfirst($namearray[0]) . "<br>";
+                  echo "<b>User Email</b>\t\t\t&emsp;", $user_email;
+                  echo "</td>";
 
-                    ?>
-                   <br>
-                   <br>
-                   <div class="mb-3">
-                     <!-- Button for updating resume -->
-                     <input type="button" value="Update Resume" class="btn btn-primary ms-auto" id="btnUpdateResume" onClick="document.location.href='/updateResume.php'" />
-                     <!-- Button for Delete Account -->
-                     <input type="button" value="Delete Account" class="btn btn-primary ms-auto" id="btnDeleteAccount" onClick="document.location.href='/deleteAccount.php'" />
-                   </div>
-                 </form>
-               </div>
-             </div>
-           </div>
-         </div>
-         <br>
-         <div align="center">Made with <span style="color: #e25555;">&hearts;</span>. Contribute on <a href="https://github.com/sak007/SRIJAS" class="text-dark" target="_blank">GitHub</a>.</div>
-         <br>
-       </div>
-     </section>
-   </div>
- </body>
+                  ?>
+                  <br>
+                  <br>
+                  <div class="mb-3">
+                    <!-- Button for updating resume -->
+                    <input type="button" value="Update Resume" class="btn btn-primary ms-auto" id="btnUpdateResume"
+                      onClick="document.location.href='/updateResume.php'" />
+                    <!-- Button for Delete Account -->
+                    <input type="button" value="Delete Account" class="btn btn-primary ms-auto" id="btnDeleteAccount"
+                      onClick="document.location.href='/deleteAccount.php'" />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div align="center">Made with <span style="color: #e25555;">&hearts;</span>. Contribute on <a
+            href="https://github.com/Swaranjali167/Jobify" class="text-dark" target="_blank">GitHub</a>.</div>
+        <br>
+      </div>
+    </section>
+  </div>
+</body>
 
- </html>
+</html>
--- code/Web_app/signup.php
+++ code/Web_app/signup.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="author" content="Group-32, Fall 2021">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>JOBBY - Signup</title>
+  <meta charset="utf-8">
+  <meta name="author" content="Group-32, Fall 2021">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>JOBIFY - Signup</title>
   <?php
   /**
-     * This segment of code is responsible for loading entries from the job_master
-     * table and putting them into the drop-down in the form
-     * @var object paramsFile and
-     * @var params associative array(string) extracts content from parameters.json
-     * @var skill_ids array(int) stores the IDs of the job_master table entries
-     * @var skill_array array(string) stores the titles of the job_master table entries.
-      */
+   * This segment of code is responsible for loading entries from the job_master
+   * table and putting them into the drop-down in the form
+   * @var object paramsFile and
+   * @var params associative array(string) extracts content from parameters.json
+   * @var skill_ids array(int) stores the IDs of the job_master table entries
+   * @var skill_array array(string) stores the titles of the job_master table entries.
+   */
   $paramsFile = file_get_contents("parameters.json");
   $params = json_decode($paramsFile, true);
-  $cities = array("Austin, Texas", "Dallas, Texas", "Raleigh, North Carolina","San Jose, California", "Charlotte, North Carolina","Seattle, Washington","San Francisco, California","Atlanta, Georgia","Huntsville, Alabama","Denver, Colorado","Washington, DC","Boulder, Colorado","Durham-Chapel Hill, North Carolina","Columbus, Ohio","Colorado Springs, Colorado","Boston, Massachusetts","Baltimore, Maryland","Madison, Wisconsin","San Diego, California","Trenton, New Jersey");
+  $cities = array("Austin, Texas", "Dallas, Texas", "Raleigh, North Carolina", "San Jose, California", "Charlotte, North Carolina", "Seattle, Washington", "San Francisco, California", "Atlanta, Georgia", "Huntsville, Alabama", "Denver, Colorado", "Washington, DC", "Boulder, Colorado", "Durham-Chapel Hill, North Carolina", "Columbus, Ohio", "Colorado Springs, Colorado", "Boston, Massachusetts", "Baltimore, Maryland", "Madison, Wisconsin", "San Diego, California", "Trenton, New Jersey");
   /**
-     * @var servername string and
-     * @var username string and
-     * @var password string and
-     * @var db string variables store the connection parameters for $conn
-     */
+   * @var servername string and
+   * @var username string and
+   * @var password string and
+   * @var db string variables store the connection parameters for $conn
+   */
   $servername = $params["server_name"];
   $username = $params["user_name"];
   $password = $params["password"];
@@ -38,181 +39,194 @@
   $skill_ids = array();
   $len = $result->num_rows;
   if ($len > 0) {
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
       array_push($skill_array, $row["job_title"]);
       array_push($skill_ids, $row["job_id"]);
     }
   }
   $conn->close();
   ?>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-	<div class="bg">
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-sm-center h-100">
-				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-          <div class="text-center my-5">
-						<img src="logo.jpg" alt="logo">
-            <!-- <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100"> -->
-            <!-- <h2 class="display-4 font-weight-bold" style="font-size:15px;display:inline-block">S.R.I.J.A.S. (Smart Resume Interpreter And Job Alerts System)</h2> -->
-          </div>
-					<div class="card shadow-lg">
-						<div class="card-body p-5">
-							<h1 class="fs-4 card-title fw-bold mb-4">Register</h1>
-							<form method="POST" class="needs-validation" novalidate="" autocomplete="off" enctype="multipart/form-data" action="registerUser.php">
+  <div class="bg">
+    <section class="h-100">
+      <div class="container h-100">
+        <div class="row justify-content-sm-center h-100">
+          <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div class="text-center my-5">
+              <img src="logos.jpg" alt="logo">
+              <!-- <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100"> -->
+              <!-- <h2 class="display-4 font-weight-bold" style="font-size:15px;display:inline-block">S.R.I.J.A.S. (Smart Resume Interpreter And Job Alerts System)</h2> -->
+            </div>
+            <div class="card shadow-lg">
+              <div class="card-body p-5">
+                <h1 class="fs-4 card-title fw-bold mb-4">Register</h1>
+                <form method="POST" class="needs-validation" novalidate="" autocomplete="off"
+                  enctype="multipart/form-data" action="registerUser.php">
 
-								<div class="mb-3">
-									<label class="mb-2 text-muted" for="inputName">Name</label>
-									<input id="inputName" type="text" class="form-control" name="inputName" placeholder="Enter your Name" value="" required autofocus>
-									<div class="invalid-feedback"> Name is required </div>
-								</div>
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="inputName">Name</label>
+                    <input id="inputName" type="text" class="form-control" name="inputName"
+                      placeholder="Enter your Name" value="" required autofocus>
+                    <div class="invalid-feedback"> Name is required </div>
+                  </div>
 
-								<div class="mb-3">
-									<label class="mb-2 text-muted" for="inputEmail">E-Mail Address</label>
-									<input id="inputEmail" type="email" class="form-control" name="inputEmail" placeholder="Enter your email address" value="" required>
-									<div class="invalid-feedback"> Email is Invalid </div>
-								</div>
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="inputEmail">E-Mail Address</label>
+                    <input id="inputEmail" type="email" class="form-control" name="inputEmail"
+                      placeholder="Enter your email address" value="" required>
+                    <div class="invalid-feedback"> Email is Invalid </div>
+                  </div>
 
-								<div class="mb-3">
-									<label class="mb-2 text-muted" for="inputLocation">Password</label>
-									<input id="password" type="password" class="form-control" name="password" placeholder="Enter your Password" required>
-                  <div class="input-group-append">
-                      <button type="button" class="btn btn-link" id="showPasswordToggle" onclick="togglePasswordVisibility()">
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="inputLocation">Password</label>
+                    <input id="password" type="password" class="form-control" name="password"
+                      placeholder="Enter your Password" required>
+                    <div class="input-group-append">
+                      <button type="button" class="btn btn-link" id="showPasswordToggle"
+                        onclick="togglePasswordVisibility()">
                         <i class="far fa-eye" id="eyeIcon"></i>
                       </button>
                     </div>
-								  <div class="invalid-feedback"> Password is required </div>
-								</div>
+                    <div class="invalid-feedback"> Password is required </div>
+                  </div>
 
-                <div class="mb-3">
-                  <label class="mb-2 text-muted" for="inputLocation">Location</label>
-                  <select class="custom-select mr-sm-2 form-control" id="location" name="location" required>
-                   <option selected>Select City...</option>
-                   <?php
-                   $count = 0;
-                   foreach($cities as $city){
-                   echo "<option value='".explode(",",$city)[0]."'>".$city."</option>";
-                   $count = $count+1;
-                   }
-                   ?>
-                  </select>
-                  <div class="invalid-feedback"> Location is required </div>
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="inputLocation">Location</label>
+                    <select class="custom-select mr-sm-2 form-control" id="location" name="location" required>
+                      <option selected>Select City...</option>
+                      <?php
+                      $count = 0;
+                      foreach ($cities as $city) {
+                        echo "<option value='" . explode(",", $city)[0] . "'>" . $city . "</option>";
+                        $count = $count + 1;
+                      }
+                      ?>
+                    </select>
+                    <div class="invalid-feedback"> Location is required </div>
+                  </div>
+
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="inputJobTypeId">Job you're looking for </label>
+                    <select class="custom-select mr-sm-2 form-control" id="inputJobTypeId" name="inputJobTypeId"
+                      required>
+                      <option selected>Choose...</option>
+                      <?php
+                      $count = 0;
+                      foreach ($skill_array as $skill) {
+                        echo "<option value='" . $skill_ids[$count] . "'>" . $skill . "</option>";
+                        $count = $count + 1;
+                      }
+                      ?>
+                    </select>
+                  </div>
+
+                  <div class="mb-3">
+                    <label class="mb-2 text-muted" for="uploadResume">Upload Your Resume</label>
+                    <input type="file" class="form-control-file" id="uploadResume" name="uploadResume" required>
+                  </div>
+
+                  <div class="align-items-center d-flex">
+                    <button type="submit" value="Submit" id="submit" name="submit" class="btn btn-primary ms-auto">
+                      Register </button>
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer py-3 border-0">
+                <div class="text-center"> Already have an account? <a href="login.php" class="text-dark">Login</a>
                 </div>
-
-                <div class="mb-3">
-                  <label class="mb-2 text-muted" for="inputJobTypeId">Job you're looking for </label>
-                  <select class="custom-select mr-sm-2 form-control" id="inputJobTypeId" name="inputJobTypeId" required>
-                    <option selected>Choose...</option>
-                    <?php
-                    $count = 0;
-                    foreach($skill_array as $skill){
-                    echo "<option value='".$skill_ids[$count]."'>".$skill."</option>";
-                    $count = $count+1;
-                    }
-                    ?>
-                  </select>
-                </div>
-
-                <div class="mb-3">
-                  <label class="mb-2 text-muted" for="uploadResume">Upload Your Resume</label>
-                  <input type="file" class="form-control-file" id="uploadResume" name="uploadResume" required>
-                </div>
-
-								<div class="align-items-center d-flex">
-									<button type="submit" value="Submit" id="submit" name="submit" class="btn btn-primary ms-auto"> Register </button>
-								</div>
-							</form>
-						</div>
-						<div class="card-footer py-3 border-0">
-							<div class="text-center"> Already have an account? <a href="login.php" class="text-dark">Login</a> </div>
-						</div>
-					</div>
-        <br><div align="center">Made with <span style="color: #e25555;">&hearts;</span>. Contribute on <a href="https://github.com/sak007/SRIJAS" class="text-dark" target="_blank">GitHub</a>.</div>
-				</div>
-			</div>
-		</div>
-	</section>
-</div>
-<script>
-  function validateForm() {
-    const name = document.getElementById('inputName').value.trim();
-    const email = document.getElementById('inputEmail').value.trim();
-    const password = document.getElementById('password').value;
-    const location = document.getElementById('location').value;
-    const jobType = document.getElementById('inputJobTypeId').value;
-    const resume = document.getElementById('uploadResume').value;
-    const resumeExtension = resume.slice(((resume.lastIndexOf(".") - 1) >>> 0) + 2);
+              </div>
+            </div>
+            <br>
+            <div align="center">Made with <span style="color: #e25555;">&hearts;</span>. Contribute on <a
+                href="https://github.com/sak007/SRIJAS" class="text-dark" target="_blank">GitHub</a>.</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <script>
+    function validateForm() {
+      const name = document.getElementById('inputName').value.trim();
+      const email = document.getElementById('inputEmail').value.trim();
+      const password = document.getElementById('password').value;
+      const location = document.getElementById('location').value;
+      const jobType = document.getElementById('inputJobTypeId').value;
+      const resume = document.getElementById('uploadResume').value;
+      const resumeExtension = resume.slice(((resume.lastIndexOf(".") - 1) >>> 0) + 2);
 
 
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const passwordPattern = /^(?=.\d)(?=.[a-zA-Z]).{8,}$/; // At least 8 characters with 1 number and 1 alphanumeric character
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      const passwordPattern = /^(?=.\d)(?=.[a-zA-Z]).{8,}$/; // At least 8 characters with 1 number and 1 alphanumeric character
 
-    document.getElementById('inputName').classList.remove('is-invalid');
-    document.getElementById('inputEmail').classList.remove('is-invalid');
-    document.getElementById('password').classList.remove('is-invalid');
-    document.getElementById('location').classList.remove('is-invalid');
-    document.getElementById('inputJobTypeId').classList.remove('is-invalid');
-    document.getElementById('uploadResume').classList.remove('is-invalid');
+      document.getElementById('inputName').classList.remove('is-invalid');
+      document.getElementById('inputEmail').classList.remove('is-invalid');
+      document.getElementById('password').classList.remove('is-invalid');
+      document.getElementById('location').classList.remove('is-invalid');
+      document.getElementById('inputJobTypeId').classList.remove('is-invalid');
+      document.getElementById('uploadResume').classList.remove('is-invalid');
 
-    if (name === '') {
-      document.getElementById('inputName').classList.add('is-invalid');
-      return false;
+      if (name === '') {
+        document.getElementById('inputName').classList.add('is-invalid');
+        return false;
+      }
+
+      if (!passwordPattern.test(password)) {
+        document.getElementById('password').classList.add('is-invalid');
+        alert('Password must be at least 8 characters with at least 1 number and 1 alphanumeric character.');
+        return false;
+      }
+
+      if (!emailPattern.test(email)) {
+        document.getElementById('inputEmail').classList.add('is-invalid');
+        return false;
+      }
+      if (location === 'Select City...') {
+        document.getElementById('location').classList.add('is-invalid');
+        alert('Location is required');
+        return false;
+      }
+      if (jobType === 'Choose...') {
+        document.getElementById('inputJobTypeId').classList.add('is-invalid');
+        alert('Job Type is required');
+        return false;
+      }
+      if (resume === '') {
+        document.getElementById('uploadResume').classList.add('is-invalid');
+        alert('Resume is required');
+        return false;
+      }
+
+      if (resumeExtension.toLowerCase() !== 'pdf') {
+        document.getElementById('uploadResume').classList.add('is-invalid');
+        alert('Resume should be in PDF format.');
+        return false;
+      }
+
+
+      return true;
     }
 
-    if (!passwordPattern.test(password)) {
-      document.getElementById('password').classList.add('is-invalid');
-      alert('Password must be at least 8 characters with at least 1 number and 1 alphanumeric character.');
-      return false;
+    function togglePasswordVisibility() {
+      const passwordField = document.getElementById('password');
+      const eyeIcon = document.getElementById('eyeIcon');
+
+      if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        eyeIcon.classList.remove('far', 'fa-eye');
+        eyeIcon.classList.add('far', 'fa-eye-slash');
+      } else {
+        passwordField.type = 'password';
+        eyeIcon.classList.remove('far', 'fa-eye-slash');
+        eyeIcon.classList.add('far', 'fa-eye');
+      }
     }
 
-    if (!emailPattern.test(email)) {
-      document.getElementById('inputEmail').classList.add('is-invalid');
-      return false;
-    }
-    if (location === 'Select City...') {
-      document.getElementById('location').classList.add('is-invalid');
-      alert('Location is required');
-      return false;
-    }
-    if (jobType === 'Choose...') {
-      document.getElementById('inputJobTypeId').classList.add('is-invalid');
-      alert('Job Type is required');
-      return false;
-    }
-    if (resume === '') {
-      document.getElementById('uploadResume').classList.add('is-invalid');
-      alert('Resume is required');
-      return false;
-    }
-
-    if (resumeExtension.toLowerCase() !== 'pdf') {
-      document.getElementById('uploadResume').classList.add('is-invalid');
-      alert('Resume should be in PDF format.');
-      return false;
-    }
-
-    
-    return true;
-  }
-
-function togglePasswordVisibility() {
-  const passwordField = document.getElementById('password');
-  const eyeIcon = document.getElementById('eyeIcon');
-
-  if (passwordField.type === 'password') {
-    passwordField.type = 'text';
-    eyeIcon.classList.remove('far', 'fa-eye');
-    eyeIcon.classList.add('far', 'fa-eye-slash');
-  } else {
-    passwordField.type = 'password';
-    eyeIcon.classList.remove('far', 'fa-eye-slash');
-    eyeIcon.classList.add('far', 'fa-eye');
-  }
-}
-
-</script>
+  </script>
 </body>
+
 </html>
